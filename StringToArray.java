b5ff862f3cad347181395119e6c61fd7592767c4
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-09-28 22:47:40 +0530

    Tried some other methods StringToArray.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- StringToArray.java
+++ StringToArray.java
@@ -12,3 +12,45 @@ public class StringToArray {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+
+/*
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	    Scanner sc = new Scanner(System.in);
+// 		System.out.println("String to array");
+// 		String s = sc.nextLine();
+// 	char arr [] = new char[s.length()];
+// 		for(int i = 0; i<s.length(); i++){
+// 		    arr[i] = s.charAt(i);
+// 		}
+// 			for(int i = 0; i<s.length(); i++){
+// 		    System.out.print(arr[i]);
+// 		}
+// 	}
+// }
+
+
+
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	    Scanner sc = new Scanner(System.in);
+// 		System.out.println("String to array");
+// 		String s = sc.nextLine();
+// 		String arr [] = new String[s.length()];
+// 		for(int i = 0; i<s.length(); i++){
+// 		    arr[i] = Character.toString(s.charAt(i));
+// 		}
+// 			for(int i = 0; i<s.length(); i++){
+// 		    System.out.print(arr[i]);
+// 		}
+// 	}
+// }
+
+
+*/

5cb91b4ce17c00b1aa802bbdab3abb06c1c91539
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-12 17:39:59 +0530

    Update correct footwearProgram.java
    The original method to get secondhighestbyprice was incorrect so i corrected it
    
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- IPA2/footwearProgram.java
+++ IPA2/footwearProgram.java
@@ -1,3 +1,124 @@
+//correct code : 
+/*
+import java.util.Scanner;
+
+class Footwear{
+    private int id;
+    private String name;
+    private String type;
+    private  int price;
+
+    public Footwear(int id,String name, String type, int price ){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+    //getter
+    public int getId(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public int getPrice(){
+        return this.price;
+    }
+}
+public class FootwearMain {
+  public static int getCountByType(Footwear [] f, String t){
+    int count = 0;
+    for (int i = 0; i < f.length; i++) {
+        if(f[i].getType().equalsIgnoreCase(t)){
+            count++;
+        }
+    }
+    return count;
+  }
+
+public static Footwear getSecondHighestPriceByBrand(Footwear[] f, String n) {
+   
+    int H = Integer.MIN_VALUE;
+    for (int i = 0; i < f.length; i++) {
+       if(f[i].getName().equalsIgnoreCase(n) && f[i].getPrice() > H){
+        H= f[i].getPrice();
+       }    
+    }
+
+    int secondH = Integer.MIN_VALUE;
+    for (int i = 0; i < f.length; i++) {
+        if(f[i].getName().equalsIgnoreCase(n) && f[i].getPrice() < H &&  f[i].getPrice() > secondH){
+         secondH = f[i].getPrice();
+        }
+     }
+
+     Footwear ans = null;
+      //as we have to return an object then, we will itterate through loop to find the value of that second higest and assign ans as f[i] as we cant return int wehn return type is object
+    //Logic :  In loop  if second higest (int we found as 'secondH') is same as f[i].getprice() then we assign that f[i] to ans as ans  = f[i];
+      for (int i = 0; i < f.length; i++) {
+          if(f[i].getName().equalsIgnoreCase(n) && f[i].getPrice() == secondH){
+            ans = f[i];
+            break;
+          }
+          
+      }
+
+    return ans;
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Footwear [] f = new Footwear[5];
+        for (int i = 0; i < f.length; i++) {
+            int a = sc.nextInt(); sc.nextLine();
+            String b =  sc.nextLine();;
+            String c =  sc.nextLine();
+            int d = sc.nextInt(); sc.nextLine();
+            f[i] = new Footwear(a, b, c, d);
+        }
+        String t = sc.nextLine();
+        String n = sc.nextLine();
+        
+        int ans1 = getCountByType(f, t);
+        Footwear ans2 = getSecondHighestPriceByBrand(f, n);
+        
+        if(ans1 != 0){
+            System.out.println(ans1);
+        }
+        else{
+            System.out.println("Footwear not available");
+        }
+        
+        if(ans2 !=null){
+            System.out.println(ans2.getId());
+            System.out.println(ans2.getName());
+            System.out.println(ans2.getPrice());
+        }
+        else{
+            System.out.println("Brand not available");
+        }
+
+        // Footwear obj = getSecondHighestPriceByBrand(f,n);
+		// if(obj!=null)
+		// {
+		// 	System.out.println(obj.getId());
+		// 	System.out.println(obj.getName());
+		// 	System.out.println(obj.getPrice());
+		// }
+		// else
+		// {
+		// 	System.out.println("Brand not available");
+		// }
+        sc.close();
+
+    }
+}
+
+*/
+
 package IPA2;
 import java.util.*;
 public class footwearProgram

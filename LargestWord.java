e562d65b632e1628e1c6d108d88d596948a457b4
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-15 18:34:07 +0530

    Update LargestWord.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- LargestWord.java
+++ LargestWord.java
@@ -3,7 +3,26 @@
  Input: He is so funny boy
  Output: funny
  */
+/*
+import  java.util.*;
+public class LargestWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ans = "";
+        String [] arr = str.split(" ");
+        int n = arr[0].length();
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i].length() > n){
+                ans = arr[i];
+                n = arr[i].length();
+            }
+        }
+        System.out.println(ans);
+    }
+}
 
+*/
 import java.util.*;
 public class LargestWord {
     public static void main(String[] args){

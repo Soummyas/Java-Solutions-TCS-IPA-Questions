12afbe5355e3f842980c9e1917f0462980f4e27a
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-16 11:53:03 +0530

    Update Marge.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- Marge.java
+++ Marge.java
@@ -7,7 +7,38 @@ Pop:Mom
 Output:
 Arijit, Ghosh, Pop:Mom
 */
+/*
+import java.util.*;
+public class Marge {
+    public static void main(String[] args) {
+        String str1 = "Arijit,Ghosh";
+        String str2 = "Pop:Mom,ABDS";
+        String ans[] = new String [0];
+        String s1 [] =str1.split(",");
+        String s2 [] =str2.split(",");
+     //add all elements of s1 in ans
+     for (int i = 0; i <s1.length; i++) {
+        ans = Arrays.copyOf(ans, ans.length + 1);
+        ans[ans.length - 1] = s1[i];
+         
+     }
 
+    //  then  add all elements of s2 in ans
+     for (int i = 0; i <s2.length; i++) {
+        ans = Arrays.copyOf(ans, ans.length + 1);
+        ans[ans.length - 1] = s2[i];
+         
+     }
+    //  for (int i = 0; i <s2.length + s1.length; i++) {
+    //    System.out.println(ans[i]); 
+    //  }
+    //  System.out.println(Arrays.toString(ans)); 
+     String n = Arrays.toString(ans);
+     String k = n.substring(1, n.length()-1);
+     System.out.println(k);
+    }
+}
+*/
 import java.util.*;
 public class Marge_Two_String
 {

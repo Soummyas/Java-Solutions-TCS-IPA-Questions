73f8eec8ab7973317a4b2643d85586a4af555f72
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-18 16:21:34 +0530

    Update Remove_Vowel.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- Remove_Vowel.java
+++ Remove_Vowel.java
@@ -3,7 +3,17 @@
  * Input : Arijit Ghosh
  * Output : rjt Ghsh
  */
-
+/*
+// String str = "Arijit Ghosh";
+        // String ans= "";
+        
+        // for (int i = 0; i < str.length(); i++) {
+        //   if(str.charAt(i) != 'A' && str.charAt(i) !=  'a' && str.charAt(i) != 'E' && str.charAt(i) !=  'e' && str.charAt(i) != 'I' && str.charAt(i) !=  'i' && str.charAt(i) != 'O' && str.charAt(i) !=  'o' && str.charAt(i) != 'U' && str.charAt(i) !=  'u' ){
+        //         ans+=str.charAt(i);
+        //   }
+        // }
+        // System.out.println(ans);
+*/
 import java.util.*;
 public class Remove_Vowel {
     public static void main(String[] args) {

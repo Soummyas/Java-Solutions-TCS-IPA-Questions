6f1244565bafae38459a562ce8dd07dca3cf98c1
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-18 15:34:27 +0530

    Update Remove2ConsecutiveLetter.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- Remove2ConsecutiveLetter.java
+++ Remove2ConsecutiveLetter.java
@@ -5,6 +5,20 @@
  * Input: abbcddeff
  * Output: abcdef
  */
+
+/*
+ String str = "aaaaaaazzaaabbcddeffa";
+       String ans = Character.toString(str.charAt(0));
+       for (int i = 1; i < str.length(); i++) {
+         char prev = str.charAt(i - 1);
+         char curr = str.charAt(i);
+         if(prev != curr){
+            ans+=curr;
+         }    
+       }
+
+       System.out.println(ans);
+*/
 /*
 
 public class Main

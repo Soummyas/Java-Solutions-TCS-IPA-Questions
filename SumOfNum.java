ca8db7c27e79536dca640b4ed69554357f54b697
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-10-24 14:10:43 +0530

    Update SumOfNum.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- SumOfNum.java
+++ SumOfNum.java
@@ -11,6 +11,75 @@
  * 2131
  */
 //https://chatgpt.com/share/66f8332f-8840-8006-84cb-4f239a59a7b3
+
+
+package smallCodes;
+// https://github.com/Soummyas/Java-Solutions-TCS-IPA-Questions/edit/main/SumOfNum.java
+// https://chatgpt.com/c/671a0644-dbcc-8008-a05a-8ee97bd52929
+// My code
+// public class SumOfNum {
+//     public static void main(String[] args) {
+//         String str = "123abc1998def10zxy31";
+//         int x =0;
+//         String numTemp = "";
+//         int sum = 0;
+//         for (int i = 0; i < str.length(); i++) {
+//            char ch = str.charAt(i);
+//            if(Character.isDigit(ch)){
+//             numTemp += Character.toString(ch);
+//            }
+//            else{
+//              x = Integer.parseInt(numTemp);
+//             sum+=x;
+//             numTemp = "0";
+            
+//            }
+//         }
+//         if (!numTemp.isEmpty()) {
+//             x = Integer.parseInt(numTemp);
+//             sum += x;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+
+// ChatGPT Code
+// package smallCodes;
+// public class SumOfNum {
+//     public static void main(String[] args) {
+//         String str = "123abc1998def10zxy5869";
+//         int x = 0;
+//         String numTemp = "";
+//         int sum = 0;
+
+//         for (int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+//             if (Character.isDigit(ch)) {
+//                 numTemp += ch;  // Append the digit to numTemp
+//             } else {
+//                 // Only parse and add if numTemp is not empty
+//                 if (!numTemp.isEmpty()) {
+//                     x = Integer.parseInt(numTemp);
+//                     sum += x;
+//                     numTemp = "";  // Reset numTemp after processing
+//                 }
+//             }
+//         }
+
+//         // After the loop, check if there's a number left in numTemp
+//         if (!numTemp.isEmpty()) {
+//             x = Integer.parseInt(numTemp);
+//             sum += x;
+//         }
+
+//         System.out.println(sum);
+//     }
+// }
+
+
+
+
 import java.util.*;
 public class SumOfNum 
 {

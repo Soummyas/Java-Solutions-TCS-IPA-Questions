07e5800b121e41317fc90b1848ace923fa76ae62
Soummyas <105837209+Soummyas@users.noreply.github.com>
2024-11-03 13:21:47 +0530

    Update Cruise.java
    Signed-off-by: Soummyas <105837209+Soummyas@users.noreply.github.com>

--- Cruise.java
+++ Cruise.java
@@ -1,6 +1,26 @@
-// https://prepinsta.com/tcs-coding-questions/question-5/
-
 /*
+
+// https://chatgpt.com/share/67272b34-3914-8006-b024-e82183d70ef7
+
+// Searched 4 sites
+
+// “Java code maximum number of guests on cruise at an instance”
+// bing.com
+
+// GitHub — csa0820/A party has been organised on a cruise. The party is organised for a limited time (T). The number of guests entering (E[i]) and leaving (L[i]) the party at every hour is represented as elements of the array. The task is to find the maximum number of guests present on the cruise at any given instance within T hours. Sample Input: 5 ---> Value of T [7,0,5,1,3] ---> E[], element of E[0] to E[N-1], where input each element is separated by new line [1,2,1,3,4] -----> L[],element of L[0] to L[N-1], where input each element is separated by new line Sample Output: 8 -----> Maximum number of guests on cruise at an instance at main · mokshithreddymettireddy/csa0820 · GitHub
+// github.com
+
+// GitHub — TCS Coding Questions | Practice Questions - PrepInsta
+// prepinsta.com
+
+// PREP INSTA — CSA0861/A party has been organised on a cruise. The party is organised for a limited time (T). The number of guests entering (E[i]) and leaving (L[i]) the party at every hour is represented as elements of the array. The task is to find the maximum number of guests present on the cruise at any given instance within T hours at main · NandaGopalaKrishna192211859/CSA0861 · GitHub
+// github.com
+
+// Programming Language Tutorials - ISNT — Maximum Number Of Guests On Cruise At An Instance Tcs With Code Examples
+// isnt.org.in
+ 
+// https://github.com/Soummyas/Java-Solutions-TCS-IPA-Questions/blob/main/Cruise.java
+// https://prepinsta.com/tcs-coding-questions/question-5/
 public class  Cruise
 {
 	public static void main(String[] args) {
